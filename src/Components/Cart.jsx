36c934f716f7a43e8dcdf15121f0556fccdef2c4
Pranav Dharme <b20095@students.iitmandi.ac.in>
2023-04-03 17:04:47 +0530

    First Commit

---
+++ src/Components/Cart.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+
+function ShoppingList() {
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingListItems')));
+
+  useEffect(() => {
+    const storedItems = localStorage.getItem('shoppingListItems');
+    if (storedItems) {
+      setItems(JSON.parse(storedItems));
+    }
+  }, []);
+
+  const handleAddItem = (itemName) => {
+    setItems([...items, itemName]);
+  };
+
+  const handleRemoveItem = (index) => {
+    const newItems = [...items.slice(0, index), ...items.slice(index + 1)];
+    setItems(newItems);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const itemName = event.target.elements.itemName.value;
+    handleAddItem(itemName);
+    event.target.reset();
+  };
+
+  useEffect(() => {
+    localStorage.setItem('shoppingListItems', JSON.stringify(items));
+  }, [items]);
+
+  return (
+    <div style={{ backgroundColor: '#ffc0cb', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+      <h1 style={{ textAlign: 'center', margin: '0 0 1rem 0' }}>Shopping List</h1>
+      <div style={{ backgroundColor: '#ffffff', width: '30%', minWidth:'17rem' ,padding: '0 2rem 0 2rem', border: '2px dashed black', marginBottom:'1rem', overflowY: 'auto' }}>
+        <h1 style={{ textAlign: 'center', marginBottom: '3rem' }}>Items To Buy</h1>
+        <form onSubmit={handleSubmit} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <input type="text" name="itemName" placeholder="Add a new item" style={{ width: '50%', minWidth:'8rem',borderBlockColor: '#000', borderBlockWidth: '1px', borderTopLeftRadius: '10px', borderBottomLeftRadius: '10px', fontSize: '1rem', height: '2rem' }} />
+          <button type="submit" style={{ backgroundColor: '#0000ff', color: 'white', border: 'none', borderTopRightRadius: '1rem', borderBottomRightRadius: '1rem', fontSize: '1rem', width: '20%', minWidth:'2rem' ,height: '2.35rem', marginLeft: '-.5rem', cursor: 'pointer' }}>Add</button>
+        </form>
+        <ul style={{ marginTop: '2rem'}}>
+          {items.map((item, index) => (
+            <li key={index} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', textAlign: 'justify', wordWrap: 'break-word' }}>
+              {item}
+              <button onClick={() => handleRemoveItem(index)} style={{ backgroundColor: 'transparent', border: 'none', color: 'red', cursor: 'pointer', fontSize: '1.5rem', fontWeight: 'bold' }}>X</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default ShoppingList;
